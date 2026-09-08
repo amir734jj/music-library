2e0d93b20ed3baeb36355809c89a6b73ca4927ce
Amir Hesamian <hesamian@uwm.edu>
2026-09-08 08:19:23 -0500

    Missed this

--- src/MusicLibrary.Api/Program.cs
+++ src/MusicLibrary.Api/Program.cs
@@ -9,6 +9,7 @@ using MusicLibrary.Api.Workers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Http.Extensions;
 using Newtonsoft.Json.Converters;
@@ -18,6 +19,7 @@ using FluentMigrator.Runner;
 using MusicLibrary.Contracts.Constants;
 using Serilog;
 using StreamRipper.Extensions;
+using StreamRipper.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((context, services, configuration) => configuration
@@ -112,6 +114,7 @@ builder.Services.Scan(scan => scan
     .AsMatchingInterface()
     .WithScopedLifetime());
 builder.Services.AddStreamRipper();
+builder.Services.AddSingleton<ILogger<IStreamRipper>>(NullLogger<IStreamRipper>.Instance);
 builder.Services.AddHttpClient<IStationDirectoryImportService, StationDirectoryImportService>(client => client.Timeout = TimeSpan.FromMinutes(5));
 builder.Services.AddHttpClient("LiveStreamProxy", client => client.Timeout = Timeout.InfiniteTimeSpan);
 builder.Services.AddSingleton<StationProbeStatusStore>();
